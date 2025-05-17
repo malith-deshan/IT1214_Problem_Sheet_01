fab4e4fa48ef0f3a23c4d931896235529e32d806
malith-deshan <mdpemasiri456@gmail.com>
2025-05-17 10:30:42 +0000

    first commit

--- 01.java
+++ 01.java
@@ -1,3 +1,11 @@
 class Student{
-  
+  private int studentId;
+  private String name;
+  private int daysAttended;
+
+  Student(int id, String name, int attend){
+    studentId=id;
+    this.name=name;
+    daysAttended=attend;
+  }
 }
